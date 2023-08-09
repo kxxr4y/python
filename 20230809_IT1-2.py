5eddcdc93ed545c0dc8b19febd739d2376f763b2
Dong-Gyun KIM <kxxr4y@gmail.com>
2023-08-09 12:15:01 +0900

    .

--- 20230809_IT1-2.py
+++ 20230809_IT1-2.py
@@ -20,4 +20,17 @@
 #     for i in range(n):
 #         print(value)
 
-print_n_times("안녕")
+# print_n_times("안녕")
+
+###키워드 매개변수
+# while True:
+#     print("안녕",end="")
+
+# def print_n_times(*values, n=2):
+#     for i in range(n):
+#         for value in values:
+#             print(value)
+#         print()
+
+# print_n_times("안녕하세요", "드럽게 어려운", "파이썬 프로그래밍", n=3)
+
