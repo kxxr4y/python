2c3463e2bee1dbe6b05c22b28344a038a461255c
Dong-Gyun KIM <kxxr4y@gmail.com>
2023-08-16 12:25:58 +0900

    .

---
+++ 20230816_IT1-2.py
@@ -0,0 +1,27 @@
+# def factorial(n):
+#     output = 1  
+#     for i in range(1, n + 1):
+#         output *= i
+#     return output
+
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+# print("5!:", factorial(5))
+# print("6!:", factorial(6))
+# print("50!:", factorial(50))
+# print("120!:", factorial(120))
+
+### 재귀 함수
+# def factorial(n):
+    # if n == 0:
+        # return 1
+    # else:
+        # return n * factorial(n - 1)
+
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+# print("5!:", factorial(5))
